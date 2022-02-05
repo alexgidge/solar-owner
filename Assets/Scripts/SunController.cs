1c0cbff8825ddeba4cb81ea322169f36b99e954f
alex <development@alexgidge.com>
2022-02-05 16:05:07 +0000

    Built game component

---
+++ Assets/Scripts/SunController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SunController : MonoBehaviour
+{
+    public GameObject SunSprite;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SunSprite.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckSOLOwnership()
+    {
+        
+    }
+}
